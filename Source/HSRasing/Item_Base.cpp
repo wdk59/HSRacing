448f3a882ba4e6f17abb7a769df6e0331c77b4b9
Yoonshin Hwang <90170221+Yoonsin@users.noreply.github.com>
2024-12-16 21:59:33 +0900

    UI Update
    -속도기
    -부스터 이펙트

---
+++ Source/HSRasing/Item_Base.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item_Base.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/RotatingMovementComponent.h"
+
+
+// Sets default values
+AItem_Base::AItem_Base()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	CollisionComp->InitSphereRadius(30.0f);
+	CollisionComp->BodyInstance.SetCollisionProfileName("OverlapAllDynamic");
+	CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &AItem_Base::OnBeginOverlap);
+	RootComponent = CollisionComp;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	RotationComp = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("RotationComp"));
+}
+
+// Called when the game starts or when spawned
+void AItem_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AItem_Base::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AItem_Base::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AHSRasingPawn* player = Cast<AHSRasingPawn>(OtherActor);
+	if (player) {
+		PlayerPickedUp(player);
+	}
+}
+
+void AItem_Base::PlayerPickedUp(AHSRasingPawn* Player) 
+{	
+	UWorld* world = GetWorld();
+	if (world) {
+		if (PickupSound) {
+			UGameplayStatics::SpawnSoundAtLocation(world, PickupSound, GetActorLocation());
+		}
+	}
+	
+	Destroy();
+}
+
---
+++ Source/HSRasing/Item_Jam.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item_Jam.h"
+#include "HSRasingPawn.h"
+
+void AItem_Jam::PlayerPickedUp(AHSRasingPawn* Player)
+{
+	Super::PlayerPickedUp(Player);
+	//Player -> AddBoosterGauge();
+}
+
+void AItem_Jam::BeginPlay()
+{
+	Super::BeginPlay();
+}
